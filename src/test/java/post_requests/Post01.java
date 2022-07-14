d753e11b361395a74ed2f44ee35add045b717559
Fatih Turker <fthturker80@gmail.com>
2022-07-14 23:21:55 +0300

    14.07.2022

--- src/test/java/post_requests/Post01.java
+++ src/test/java/post_requests/Post01.java
@@ -6,49 +6,57 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 
-public class Post01 extends JsonPlaceHolderBaseUrl{
-/*
-         Given
-           1) https://jsonplaceholder.typicode.com/todos
-           2)  {
-                 "userId": 55,
-                 "title": "Tidy your room",
-                 "completed": false
-              }
-        When
-            I send POST Request to the Url
-        Then
-            Status code is 201
-        And
-            response body is like {
-                                    "userId": 55,
-                                    "title": "Tidy your room",
-                                    "completed": false,
-                                    "id": 201
-                                    }
-     */
+public class Post01 extends JsonPlaceHolderBaseUrl {
+    /*
+             Given
+               1) https://jsonplaceholder.typicode.com/todos
+               2)  {
+                     "userId": 55,
+                     "title": "Tidy your room",
+                     "completed": false
+                  }
+            When
+                I send POST Request to the Url
+            Then
+                Status code is 201
+            And
+                response body is like {
+                                        "userId": 55,
+                                        "title": "Tidy your room",
+                                        "completed": false,
+                                        "id": 201
+                                        }
+         */
     @Test
-    public void post01(){
+    public void post01() {
         // 1. Set the Url
-        spec.pathParam("first","todos");
+        spec.pathParam("first", "todos");
 
         // 2. Set the expected data
-        JsonPlaceHolderTestData expectedData= new JsonPlaceHolderTestData();
-        Map<String,Object>expecteddataMap= expectedData.expectedDataWithAllKeys(55,"Tidy your room",false);
+        JsonPlaceHolderTestData expectedData = new JsonPlaceHolderTestData();
+        Map<String, Object> expecteddataMap = expectedData.expectedDataWithAllKeys(55, "Tidy your room", false);
 
         // 3. step: send post request and get Response
-        Response response=given().
+        Response response = given().
                 spec(spec).
                 contentType(ContentType.JSON).
                 body(expecteddataMap).
                 when().
                 post("/{first}");
         response.prettyPrint();
+
+        //4. Step: Do Assertion
+        Map<String, Object> actualDataMap = response.as(HashMap.class);
+        assertEquals(expecteddataMap.get("userId"),actualDataMap.get("userId"));
+        assertEquals(expecteddataMap.get("title"),actualDataMap.get("title"));
+        assertEquals(expecteddataMap.get("completed"),actualDataMap.get("completed"));
 
     }
 }
