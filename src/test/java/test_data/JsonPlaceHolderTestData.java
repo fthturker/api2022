178f479f9b48b1fda55c3305d7719e1644cce9bc
Fatih Turker <fthturker80@gmail.com>
2022-07-21 23:29:13 +0300

    21.07.2022

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -4,36 +4,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JsonPlaceHolderTestData {
-    public Map<String, Object> expectedDataWithAllKeys(Integer userId,String title, Boolean completed) {
-        Map<String, Object> expectedData= new HashMap<>();
-        expectedData.put("userId",userId);
-        expectedData.put("title",title);
-        expectedData.put("completed",completed);
+    public Map<String, Object> expectedDataWithAllKeys(Integer userId, String title, Boolean completed) {
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId", userId);
+        expectedData.put("title", title);
+        expectedData.put("completed", completed);
 
         return expectedData;
     }
-    public Map<String, Object> expectedDataWithMissingKeys(Integer userId,String title, Boolean completed){
-        Map<String, Object> expectedData= new HashMap<>();
 
-        if (userId!=null){
-            expectedData.put("userId",userId);
+    public Map<String, Object> expectedDataWithMissingKeys(Integer userId, String title, Boolean completed) {
+        Map<String, Object> expectedData = new HashMap<>();
+
+        if (userId != null) {
+            expectedData.put("userId", userId);
         }
-        if (title!=null){
-            expectedData.put("title",title);
+        if (title != null) {
+            expectedData.put("title", title);
         }
-        if (completed!=null){
-            expectedData.put("completed",completed);
+        if (completed != null) {
+            expectedData.put("completed", completed);
         }
 
         return expectedData;
     }
-    /*
-    public String expectedDataInString(){
-        String expectedData=
 
+    public String expectedDataInString(Integer userId, String title, Boolean completed) {
 
-       return
+            String expectedData = "{\n" +
+                    "\"userId\": " + userId + ",\n" +
+                    "\"title\": \"" + title + "\",\n" +
+                    "\"completed\": " + completed + "\n" +
+                    " }";
+            return expectedData;
+        }
     }
 
-     */
-}
+
+
